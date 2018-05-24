321d39000baf3f792cb5df37abbca5a3d502b979
Léopold Houdin <leopold.houdin@gmail.com>
2018-05-24 18:06:37 +0200

    ok

--- tillerinterview/__init__.py
+++ tillerinterview/__init__.py
@@ -1,4 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask rops test'
 
 if __name__ == '__main__':
-	print 'ok'
+    app.run(debug=True,host='0.0.0.0')
 
